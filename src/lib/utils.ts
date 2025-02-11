2fc4fc85876d9e72657991ae7f4667266ae0f2b3
smyaseen <smyaseen164@gmail.com>
2025-02-11 09:15:37 +0500

    fix: reduced fade-in animation causing faded effect even after load on firefox

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const fadeIn = "animate-in fade-in duration-1000 fill-mode-both";
+export const fadeIn = "animate-in fade-in duration-200 fill-mode-both";
 
 export function validateEnvVars() {
   const requiredEnvVars = ["HASHNODE_HOST", "NEXT_PUBLIC_HASHNODE_PROJECT_SERIES_SLUG"];
