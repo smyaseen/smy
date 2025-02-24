dc2e933c10c748746fdb7e033a179c13aa557305
smyaseen <smyaseen164@gmail.com>
2025-02-24 10:37:40 +0500

    build: prevent home route from caching

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,15 @@ import { NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
 
+  console.log("Middleware", url.pathname);
+  // Set Cache-Control header for the root path
+  if (url.pathname === "/") {
+    const response = NextResponse.next();
+    response.headers.set("Cache-Control", "no-store");
+    return response;
+  }
+
+  // Rewrite URLs ending with .md to the corresponding API route
   if (url.pathname.endsWith(".md")) {
     url.pathname = `/api${url.pathname}`;
     return NextResponse.rewrite(url);
@@ -13,5 +22,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/blog/:path*.md", "/projects/:path*.md"],
+  matcher: ["/", "/blog/:path*.md", "/projects/:path*.md"],
 };
