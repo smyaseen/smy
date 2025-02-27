03e5780cdba90ff299f101950319bca79b93b238
smyaseen <smyaseen164@gmail.com>
2025-02-27 09:04:44 +0500

    feat: no cache root page

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,10 @@ export function middleware(req: NextRequest) {
   // Set Cache-Control header for the root path
   if (url.pathname === "/") {
     const response = NextResponse.next();
-    response.headers.set("Cache-Control", "no-store");
+    response.headers.set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+    response.headers.set("Pragma", "no-cache");
+    response.headers.set("Expires", "0");
+    response.headers.set("Surrogate-Control", "no-store");
     return response;
   }
 
