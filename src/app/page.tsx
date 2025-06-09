dbda22d1b7dd4dce97e375d0f18caf6be9ec4836
smyaseen <smyaseen164@gmail.com>
2025-06-09 18:44:56 +0500

    style: simplified landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import AllPostsList from "@/features/posts/components/all-post-list";
 import { cn, fadeIn } from "@/lib/utils";
 import { ArrowRightIcon, EnvelopeOpenIcon, FileTextIcon, GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 import { Metadata } from "next";
-import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
 
@@ -49,26 +48,16 @@ export const metadata: Metadata = {
 export default async function Page() {
   return (
     <main className="flex flex-col gap-4 text-center items-center sm:gap-8">
-      <section className={cn(fadeIn, "animation-delay-200")}>
-        <Image className="hidden dark:block object-scale-down" src={"/bg-dark.png"} alt="dark-mode-image" width={850} height={850} />
-        <Image className="block dark:hidden object-scale-down" src={"/bg-light.png"} alt="light-mode-image" width={850} height={850} />
+      <section className="text-center max-w-xl space-y-4">
+        <h1 className="text-5xl font-extrabold tracking-tight leading-tight">SMY</h1>
+        <h2 className="text-sm font-light text-muted-foreground sm:text-lg">Syed Muhammad Yaseen</h2>
+        <p className="text-gray-500 dark:text-gray-200 text-lg sm:text-xl max-w-md mx-auto leading-relaxed">
+          Full Stack Engineer experienced in Customer Engagement & Health Tech
+        </p>
+
+        <p>JavaScript / TypeScript ecosystem, React, Next.js, NestJS, AWS</p>
       </section>
-      <section
-        className={cn(fadeIn, "animation-delay-400 px-4 md:px-40 flex flex-col md:flex-row w-full justify-start items-center gap-4 md:gap-12")}
-      >
-        <div className="text-center w-full">
-          <h1 className="text-xl font-bold sm:text-4xl">SMY</h1>
-          <h2 className="text-sm font-light text-muted-foreground sm:text-lg">Syed Muhammad Yaseen</h2>
-        </div>
-      </section>
-      <section className={cn(fadeIn, "animation-delay-600", "px-4")}>
-        <span className="text-lg sm:text-xl">
-          <div>
-            Full Stack ❯ Customer Engagement / Health Tech ❯ JavaScript / TypeScript Ecosystem ❯ React, NextJS, NestJS, NodeJS, Microservices, AWS ❯
-            BS Computer Science 🎓 ❯ Passionate about Quality Software Engineering ❯ Sharing Insights & Experiences
-          </div>
-        </span>
-      </section>
+
       <section className={cn(fadeIn, "grid grid-cols-2 md:grid-cols-4 gap-8 animation-delay-600")}>
         <Button asChild size="lg">
           <Link href="https://docs.google.com/document/d/1ukQWNh2V1B5g9lDYLS0mno0kWYb0FqMHi8JQxa8Dx5I/edit?usp=sharing" target="_blank">
