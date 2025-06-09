bcb87ba5e6b4f03b579906c13a0a2f695b89fca7
smyaseen <smyaseen164@gmail.com>
2025-06-09 13:24:37 +0500

    style: removed shadow from mode toggle

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -12,7 +12,7 @@ export function ModeToggle() {
     <Button
       variant="outline"
       size="icon"
-      className="border-0 hover:bg-background"
+      className="border-0 hover:bg-background shadow-none"
       onClick={() => {
         setTheme(theme === "dark" ? "light" : "dark");
       }}
